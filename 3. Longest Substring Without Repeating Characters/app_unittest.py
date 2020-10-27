0e382cdd4dcc4fe8b679b71f8caa426e0b0aa6fe
msopiha <Mykola.Sopiha@dataart.com>
2020-10-27 20:52:20 +0200

    small fix

--- 3. Longest Substring Without Repeating Characters/app_unittest.py
+++ 3. Longest Substring Without Repeating Characters/app_unittest.py
@@ -6,7 +6,7 @@ class TestLongestSubstringWithoutRepeating(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test1_length_of_longest_substring(self):
+    def test_length_of_longest_substring(self):
         self.assertEqual(self.solution.lengthOfLongestSubstring('abcabcbb'), 3)
         self.assertEqual(self.solution.lengthOfLongestSubstring('bbbbb'),    1)
         self.assertEqual(self.solution.lengthOfLongestSubstring('pwwkew'),   3)
