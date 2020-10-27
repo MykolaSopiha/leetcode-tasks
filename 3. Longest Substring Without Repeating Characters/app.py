6f3a02b6b310c2243b6079d34e262b9de67f3f4c
msopiha <Mykola.Sopiha@dataart.com>
2020-10-27 20:48:53 +0200

    3. Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters/app.py
+++ 3. Longest Substring Without Repeating Characters/app.py
@@ -2,11 +2,12 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         start = 0
         maxLength = 0
-        
+
         for end in range(len(s)):
             if s[end] in s[start:end]:
                 while s[end] in s[start:end]:
                     start += 1
-            maxLength = max(end - start + 1, maxLength)
-        
+            else:
+                maxLength = max(maxLength, end - start + 1)
+
         return maxLength
--- 3. Longest Substring Without Repeating Characters/app_unittest.py
+++ 3. Longest Substring Without Repeating Characters/app_unittest.py
@@ -7,18 +7,9 @@ class TestLongestSubstringWithoutRepeating(unittest.TestCase):
         self.solution = Solution()
 
     def test1_length_of_longest_substring(self):
-        self.assertEqual(
-            self.solution.lengthOfLongestSubstring('abcabcbb'),
-            3
-        )
-        self.assertEqual(
-            self.solution.lengthOfLongestSubstring('bbbbb'),
-            1
-        )
-        self.assertEqual(
-            self.solution.lengthOfLongestSubstring('pwwkew'),
-            3
-        )
+        self.assertEqual(self.solution.lengthOfLongestSubstring('abcabcbb'), 3)
+        self.assertEqual(self.solution.lengthOfLongestSubstring('bbbbb'),    1)
+        self.assertEqual(self.solution.lengthOfLongestSubstring('pwwkew'),   3)
 
 
 if __name__ == '__main__':
