c0017b8e3638d0a868b50daf514376101e0f0749
Mykola Sopiha <sopiha.mv@gmail.com>
2021-09-07 16:42:14 +0300

    101. Symmetric Tree (recursive solution)

---
+++ 101. Symmetric Tree/app.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from data_structures import BinaryTreeNode as TreeNode
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isMirror(root.left, root.right)
+    
+    def isMirror(self, left, right):
+        if left == None and right == None:
+            return True
+        elif left != None and right != None:
+            return (
+                left.val == right.val
+                and self.isMirror(left.left, right.right)
+                and self.isMirror(left.right, left.right)
+             )
+            
