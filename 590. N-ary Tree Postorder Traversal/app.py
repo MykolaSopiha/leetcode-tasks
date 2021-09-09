ebdcd4f182dfeae56625b056e1c61d5264f935ac
Mykola Sopiha <sopiha.mv@gmail.com>
2021-09-09 17:56:47 +0300

    590. N-ary Tree Postorder Traversal

---
+++ 590. N-ary Tree Postorder Traversal/app.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def postorder(self, root: Node) -> List[int]:
+        if not root: return []
+        result = []
+        for child in root.children:
+            result += self.postorder(child)
+        return result + [root.val]
